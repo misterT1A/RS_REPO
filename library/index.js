5591500343ab0dce5f2d082ab198c5ef449e3239
misterT1A <paradogss@.mail.ru>
2023-08-04 13:26:59 +0300

    feat: add burger menu and script

--- library/index.js
+++ library/index.js
@@ -1,1 +1,32 @@
 console.log("100/100")
+
+// burger menu
+
+const nav = document.getElementById('nav');
+const navWrapper = document.getElementById('navWrapper');
+const burgerButton = document.getElementById('burgerButton');
+const burgerButtonWrapper = document.getElementById('burgerIcon');
+const navLinks = document.querySelectorAll('.navLink');
+
+burgerButtonWrapper.addEventListener('click', () => {
+    nav.classList.toggle('nav_open');
+    navWrapper.classList.toggle('nav_active');
+    burgerButton.classList.toggle('burger_close');
+})
+
+navWrapper.addEventListener('click', function (e) {
+    if (e.target.classList.contains('nav_active')) {
+        nav.classList.toggle('nav_open');
+        navWrapper.classList.toggle('nav_active');
+        burgerButton.classList.toggle('burger_close');
+    }
+})
+
+navLinks.forEach(elem => {
+    elem.addEventListener('click', () => {
+        nav.classList.toggle('nav_open');
+        navWrapper.classList.toggle('nav_active');
+        burgerButton.classList.toggle('burger_close');
+    })
+})
+
