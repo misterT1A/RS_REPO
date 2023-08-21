3ed65715398dc26f92138f919b6c26d3c290c0b9
misterT1A <paradogss@.mail.ru>
2023-08-21 23:54:56 +0300

    fix: profile menu

---
+++ library/index2.js
@@ -0,0 +1,12 @@
+const profileMenu = document.querySelector('.profile_menu');
+const profileIcon = document.getElementById('profileIcon');
+
+profileIcon.addEventListener('click', () => {
+    profileMenu.classList.toggle('profile_open')
+})
+
+
+
+// if(profileMenu.classList.contains('profile_open')) {
+
+// }
