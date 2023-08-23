70b54e00b0a807c8fece78e3f65add48a9246a77
misterT1A <paradogss@.mail.ru>
2023-08-24 01:42:07 +0300

    fix: profile menu add js logic

--- library/index2.js
+++ library/index2.js
@@ -1,12 +1,25 @@
 const profileMenu = document.querySelector('.profile_menu');
 const profileIcon = document.getElementById('profileIcon');
+const body2 = document.getElementById('body');
 
+//profileMenu===================
+let openMenu = false;
 profileIcon.addEventListener('click', () => {
-    profileMenu.classList.toggle('profile_open')
+    if(!openMenu) {
+        profileMenu.classList.add('profile_open')
+        setTimeout(() => {
+            openMenu = true;
+        }, 300)
+    } else {
+        profileMenu.classList.remove('profile_open')
+        openMenu = false;
+    }
 })
 
-
-
-// if(profileMenu.classList.contains('profile_open')) {
-
-// }
+body2.addEventListener('click', (elem) => {
+    if (openMenu && elem.target != profileMenu) {
+        profileMenu.classList.remove('profile_open')
+        openMenu = false;
+    }
+})
+////====================
