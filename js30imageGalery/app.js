b856e1e4e74cd6e3e195e37c4bbab977aa8ef05a
misterT1A <paradogss@mail.ru>
2023-09-21 12:17:37 +0300

    feat: get api reqest

--- js30imageGalery/app.js
+++ js30imageGalery/app.js
@@ -3,6 +3,9 @@ const searchIcon = document.querySelector('.search_icon');
 const closeIcon = document.querySelector('.close_icon');
 
 const galery = document.querySelector('.galery_inner');
+const body = document.querySelector('.body');
+
+let imgs = document.querySelectorAll('img');
 
 //search btn====================
 let fullInput = false;
@@ -22,12 +25,32 @@ closeIcon.addEventListener('click', () => {
 })
 //=============================
 
-const url = 'https://api.unsplash.com/search/photos?query=spring&client_id=IW3Wgnd6Xw3b6ph-2LHMOko8HT4BuIkFPVIFtEoCJFU';
+// const url = 'https://api.unsplash.com/search/photos?query=winter&per_page=30&client_id=IW3Wgnd6Xw3b6ph-2LHMOko8HT4BuIkFPVIFtEoCJFU';
+// const url = 'https://type.fit/api/quotes';
 
+// async function getData() {
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     console.log(data);
+//     galery.innerHTML = '';
+//     // let dataUrls = data.results.map(elem => {
 
-async function getData() {
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data);
+//     //     console.log(elem.urls.regular)
+
+//     //     createImg(elem.urls.regular)
+//     // })
+//     localStorage.setItem('data', JSON.stringify(data));
+// }
+// getData();
+let data = JSON.parse(localStorage.getItem('data'));
+galery.innerHTML = '';
+let arrUrl = data.results.map(elem => {
+    createImg(elem.urls.regular)
+})
+
+function createImg(url) {
+    const element = document.createElement('div');
+    element.classList.add('img');
+    element.style.backgroundImage = `url('${url}')`;
+    galery.append(element)
 }
-getData();
